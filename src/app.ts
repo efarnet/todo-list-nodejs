11160413469c7a02babc70c3236ac9bac85af4d1
efarnet <74194292+efarnet@users.noreply.github.com>
2025-09-12 10:24:36 +0200

    Feature/db tests (#1)
    * :arrow_up:
    
    * :white_check_mark: db connection test

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-connectDB({});
+connectDB({ mongoURI: process.env.MONGO_URI || "" });
 
 // Middleware
 app.use(express.json());
---
+++ src/tests/db.test.ts
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+import connectDB from "../config/db";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+jest.mock("mongoose");
+const mockedMongoose = mongoose as jest.Mocked<typeof mongoose>;
+
+test("database connection successful", async () => {
+  mockedMongoose.connect.mockResolvedValueOnce({
+    connection: { host: "localhost" },
+  } as any);
+
+  await connectDB({
+    mongoURI: process.env.MONGO_URI,
+    maxRetries: 3,
+    retryDelay: 10,
+  });
+
+  expect(mockedMongoose.connect).toHaveBeenCalledTimes(1);
+  expect(mockedMongoose.connect).toHaveBeenCalledWith(process.env.MONGO_URI);
+});
+
+describe("connectDB retry system", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test("database connection failed with retries", async () => {
+    mockedMongoose.connect.mockRejectedValue(new Error("DB Error"));
+
+    await connectDB({
+      mongoURI: "mongodb://fail",
+      maxRetries: 3,
+      retryDelay: 10,
+    });
+
+    expect(mockedMongoose.connect).toHaveBeenCalledTimes(3);
+  });
+});
